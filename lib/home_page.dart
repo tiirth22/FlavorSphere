1d7f0dce78c2e432dc8915a35472ac13a556affc
Tirth Jignesh Dalal <152633332+tiirth22@users.noreply.github.com>
2024-09-03 18:01:40 +0530

    Create home_page.dart
    added just a demo home page

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Home Page!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
