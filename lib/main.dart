0629c7fb9df1b049b9241a728a87d3c44aaeb58e
Kashish <kashishgandhi.278@gmail.com>
2024-09-06 09:23:13 +0530

    debug banner remove

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // This removes the debug banner
       title: 'FlavorSphere',
       theme: ThemeData(
         primarySwatch: Colors.orange,
